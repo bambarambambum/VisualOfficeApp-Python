627f17b903f2a01f54f9999d64b4dc99f1ca39e6
Mikhail Androsov <bambarambambum@yandex.ru>
2021-09-08 21:11:26 +0300

    Create edit.html + edit function (only get)

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -15,5 +15,16 @@ def main():
         print("Ошибка чтения из БД")
     return render_template('index.html', title = "Главная", managers = managers)
 
+@app.route('/edit/<int:id>')
+def edit(id):
+    user = ''
+    manager = ''
+    try:
+        user = json.loads(requests.get(BASE + "/api/users/" + str(id)).text)
+        manager = json.loads(requests.get(BASE + "/api/managers/" + str(user["manager_id"])).text)
+    except:
+        print("Ошибка чтения из БД")
+    return render_template('edit.html', title = "Редактировать", user = user, manager = manager)
+
 if __name__ == '__main__':
     app.run(port=5000)
