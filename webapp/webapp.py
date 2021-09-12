f22245d4f174810041fe8e26049630dfab6380d0
Mikhail Androsov <bambarambambum@yandex.ru>
2021-09-12 20:23:22 +0300

    add config and options in webapp.py

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template, request
 import json
 import requests
+from configparser import ConfigParser
+from pathlib import Path
+
+script_path = Path(__file__).absolute().parent
+
+# Configuration
+file = f"{script_path}/config"
+config = ConfigParser()
+config.read(file)
+READER_HOST = config.get('users_reader','host')
+READER_PORT = config.get('users_reader','port')
+WRITER_HOST = config.get('users_writer','host')
+WRITER_PORT = config.get('users_writer','port')
 
 app = Flask(__name__)
 
-BASE = "http://localhost:5001"
+BASE = f"http://{READER_HOST}:{READER_PORT}"
 
 @app.route('/')
 def main():
@@ -34,7 +47,7 @@ def save(id):
     try:
         user = request.form.to_dict()
         manager = json.loads(requests.get(BASE + "/api/managers/" + str(user["manager_id"])).text)
-        url = 'http://localhost:5000/api/users/' + user['id']
+        url = f'http://{WRITER_HOST}:{WRITER_PORT}/api/users/' + user['id']
         requests.post(url, data=user)
     except:
         print("Ошибка чтения из БД")
